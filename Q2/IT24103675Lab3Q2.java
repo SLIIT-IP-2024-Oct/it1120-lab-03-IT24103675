df99faf0a7facdb5bacbd659d32a379e5eb92030
IT24103675 <it24103675@my.sliit.lk>
2024-11-09 21:36:41 +0530

    Update IT24103675Lab3Q2.java

--- Q2/IT24103675Lab3Q2.java
+++ Q2/IT24103675Lab3Q2.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
+public class IT24103675Lab3Q2{
+public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter the Amount of salary:");
+    double amount = input.nextDouble();
+    System.out.println("The amount of the salary is: "+amount);
+    System.out.println("Enter the OT hours: ");
+    double OThours = input.nextDouble();
+    System.out.println("The OT hours: "+OThours);
+    double OTrate =input.nextDouble();
+    System.out.println("The Ot rate: "+OTrate);
 
-public class  IT24103675Lab3Q2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter the total bill amount: ");
-        double totalBill = input.nextDouble();
-
-        // Calculate the discount amount
-        double discouAmou = totalBill * 0.1;
-
-        // Calculate the final amount after discount
-        double finalAmou = totalBill - discouAmou;
-
-        System.out.println("Discount amount is = Rs" + discouAmou);
-        System.out.println("Final amount after discount is = Rs" + finalAmou);
-    }
+    double OT_Salary = (OThours*OTrate);
+    double Tot_Salary = (amount + OT_Salary);
+    System.out.println("The total salary is; "+Tot_Salary);
+}
 }
